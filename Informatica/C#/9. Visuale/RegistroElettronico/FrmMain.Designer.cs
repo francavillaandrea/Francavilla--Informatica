d9cc974af63d4a296fc3f650ae8105faab68aa67
Andrea Francavilla <a.francavilla.3537@vallauri.edu>
2025-02-03 08:55:53 +0100

    Registro Elettronico

---
+++ Informatica/C#/9. Visuale/RegistroElettronico/FrmMain.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace RegistroElettronico
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgv.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(820, 705);
+            this.dgv.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 729);
+            this.Controls.Add(this.dgv);
+            this.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "REGISTRO ELETTRONICO";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ Informatica/C#/9. Visuale/RegistroElettronico/FrmMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroElettronico
+{
+    public partial class FrmMain : Form
+    {
+        static Random rnd = new Random();
+        string[] cognomi = { "Barberis", "Bedreag", "Cambieri",
+            "Cane", "Casalis", "Cebotari", "Dellaferrera", "Ferrero",
+            "Francavilla", "Gatto", "Genta", "Ingaramo", "Manunta",
+            "Marciuc", "Milojkov", "Monte", "Oricco", "Ressia", "Revello",
+            "Riso", "Roveta", "Salinitri", "Ufe" };
+        string[] int_materie = { "ITA", "STO", "MAT", "ING", "INF",
+                                "SIS", "TPS", "TEL" };
+        int[,] voti;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            settaDGV();
+            generaVoti();
+            StampaMatriceinDgv();
+        }
+
+        private void StampaMatriceinDgv()
+        {
+            for (int i = 0; i < voti.GetLength(0); i++)
+                for (int j = 0; j < voti.GetLength(1); j++)
+                {
+                    dgv.Rows[i].Cells[j].Value = voti[i, j].ToString();
+                    if (voti[i, j] == 5)
+                        dgv.Rows[i].Cells[j].Style.BackColor = Color.Yellow;
+                    else if (voti[i, j] < 5)
+                    {
+                        dgv.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                    }
+                    else
+                        dgv.Rows[i].Cells[j].Style.BackColor = Color.Green;
+                }
+        }
+
+        private void generaVoti()
+        {
+            for (int i = 0; i < voti.GetLength(0); i++)
+                for (int j = 0; j < voti.GetLength(1); j++)
+                    voti[i, j] = rnd.Next(1, 11);
+        }
+
+        private void settaDGV()
+        {
+            int i;
+            // Istanza della matrice 
+            voti = new int[cognomi.Length, int_materie.Length];
+            dgv.RowCount = voti.GetLength(0);
+            dgv.ColumnCount = voti.GetLength(1);
+            dgv.ReadOnly = true;
+
+            /* Intestazione delle colonne */
+            for (i = 0; i < int_materie.Length; i++)
+                dgv.Columns[i].HeaderText = int_materie[i];
+            dgv.Columns[i].HeaderText = "Media";
+            i++;
+            dgv.Columns[i].HeaderText = "Esito";
+            i++;
+            dgv.Columns[i].HeaderText = "Recupero";
+
+            /* Intestazione delle righe */
+            for (i = 0; i < cognomi.Length; i++)
+                dgv.Rows[i].HeaderCell.Value = cognomi[i];
+
+            dgv.ClearSelection(); // Rimuove selezione di default (0, 0) 
+
+            // Istruzioni per resize 
+            dgv.AutoSizeColumnsMode =
+                            DataGridViewAutoSizeColumnsMode.AllCells;
+            dgv.AutoSizeRowsMode =
+                            DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            dgv.RowHeadersWidthSizeMode =
+                            DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+        }
+    }
+}
