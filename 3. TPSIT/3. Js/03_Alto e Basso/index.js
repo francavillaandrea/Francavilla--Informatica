7a42b67940e64c43157cbd931f7ca56f970414bf
Andrea Francavilla <a.francavilla.3537@vallauri.edu>
2025-02-11 08:49:29 +0100

    Update Dispense

--- 3. TPSIT/3. Js/03_Alto e Basso/index.js
+++ 3. TPSIT/3. Js/03_Alto e Basso/index.js
@@ -3,18 +3,6 @@ let nSegreto = generaNumero(1,101);
 let cont;
 let _txtNumero = document.getElementById("txtNumero");
 
-
-
-
-
-
-
-
-
-
-
-
-
 function generaNumero(min,max){
     /*formula per generare un numero 
     casuale compreso tra min e max max escluso */
