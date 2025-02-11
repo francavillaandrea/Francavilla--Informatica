433f57987efd4b74e3c9f7746c34e31df6e38902
Andrea Francavilla <a.francavilla.3537@vallauri.edu>
2025-02-11 09:44:37 +0100

    js

--- 3. TPSIT/3. Js/03_Alto e Basso/index.js
+++ 3. TPSIT/3. Js/03_Alto e Basso/index.js
@@ -1,11 +1,59 @@
 "use strict"
 let nSegreto = generaNumero(1,101);
-let cont;
 let _txtNumero = document.getElementById("txtNumero");
-
+let _divMsg = document.getElementById("divMessaggio");
+let _btnGioca = document.getElementById("btnGioca");
+let _btnReset = document.getElementById("btnReset");
+let cntTent = 0;
+_btnReset.style
 function generaNumero(min,max){
     /*formula per generare un numero 
     casuale compreso tra min e max max escluso */
-    let n = Math.floor((max-min)*Math.random()+min);
-    return n;    
+    return Math.floor((max-min)*Math.random()+min);
 }
+function gioca ()
+{
+    let msg = "";
+    let nUser = parseInt(_txtNumero.value);
+    let win = false;
+    if(nUser <1 || nUser >= 100 || _txtNumero.value === "" )
+    {
+        alert("numero non valido");
+       
+    }
+    else
+    {   
+        cntTent++;
+        divTries.textContent = "Tentativi: " + cntTent.toString();
+        if(nUser > nSegreto)
+        {
+            msg = "numero inserito troppo grande " + nUser.toString();
+        }
+        else if(nUser < nSegreto)
+        {
+            msg = "numero inserito troppo piccolo " + nUser.toString();
+        }
+        else
+        {
+            msg = "Numero indovinato!";
+            win = true;
+            terminaGioco();
+            if(!winb && cntTent >= 10)
+            {
+                msg += "<br> Hai perso, hai esaurito i tentativi. Il numero segreto era "+ nSegreto.toString();
+                terminaGioco();
+            }
+            
+        }
+        _divMsg.innerHTML += msg + "<br>";
+    }
+    _txtNumero.value = " ";
+    _txtNumero.focus();
+}
+function terminaGioco()
+{
+    _txtNumero.disabled = true;
+    _btnGioca.disabled = true;
+    _btnReset
+}
+
