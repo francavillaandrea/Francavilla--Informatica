445436af7bbe7795817cfc5f8e92be0c720f47e8
Andrea Francavilla <a.francavilla.3537@vallauri.edu>
2025-02-26 08:49:26 +0100

    Info

---
+++ 1. Informatica/C#/9. Visuale/GestioneTabella3B/frmMain.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace GetioneTabella3B
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCaricaTabellaStudenti = new System.Windows.Forms.Button();
+            this.dgvStudenti = new System.Windows.Forms.DataGridView();
+            this.btnOrdinaStudentiCognomeNome = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRicercaBinariaMatricola = new System.Windows.Forms.Button();
+            this.btnCercaClassePiùStudenti = new System.Windows.Forms.Button();
+            this.btnContaStudentiClasse = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dgvValutazioni = new System.Windows.Forms.DataGridView();
+            this.btnCaricaValutazioni = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudenti)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvValutazioni)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCaricaTabellaStudenti
+            // 
+            this.btnCaricaTabellaStudenti.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaricaTabellaStudenti.Location = new System.Drawing.Point(12, 12);
+            this.btnCaricaTabellaStudenti.Name = "btnCaricaTabellaStudenti";
+            this.btnCaricaTabellaStudenti.Size = new System.Drawing.Size(163, 23);
+            this.btnCaricaTabellaStudenti.TabIndex = 0;
+            this.btnCaricaTabellaStudenti.Text = "CARICA STUDENTI";
+            this.btnCaricaTabellaStudenti.UseVisualStyleBackColor = true;
+            this.btnCaricaTabellaStudenti.Click += new System.EventHandler(this.btnCaricaTabellaStudenti_Click);
+            // 
+            // dgvStudenti
+            // 
+            this.dgvStudenti.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvStudenti.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvStudenti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudenti.Location = new System.Drawing.Point(12, 47);
+            this.dgvStudenti.Name = "dgvStudenti";
+            this.dgvStudenti.Size = new System.Drawing.Size(446, 150);
+            this.dgvStudenti.TabIndex = 1;
+            // 
+            // btnOrdinaStudentiCognomeNome
+            // 
+            this.btnOrdinaStudentiCognomeNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdinaStudentiCognomeNome.Location = new System.Drawing.Point(17, 19);
+            this.btnOrdinaStudentiCognomeNome.Name = "btnOrdinaStudentiCognomeNome";
+            this.btnOrdinaStudentiCognomeNome.Size = new System.Drawing.Size(163, 42);
+            this.btnOrdinaStudentiCognomeNome.TabIndex = 2;
+            this.btnOrdinaStudentiCognomeNome.Text = "ORDINA STUDENTI NOMINATIVO";
+            this.btnOrdinaStudentiCognomeNome.UseVisualStyleBackColor = true;
+            this.btnOrdinaStudentiCognomeNome.Click += new System.EventHandler(this.btnOrdinaStudentiCognomeNome_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRicercaBinariaMatricola);
+            this.groupBox1.Controls.Add(this.btnCercaClassePiùStudenti);
+            this.groupBox1.Controls.Add(this.btnContaStudentiClasse);
+            this.groupBox1.Controls.Add(this.btnOrdinaStudentiCognomeNome);
+            this.groupBox1.Location = new System.Drawing.Point(464, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 208);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Query";
+            // 
+            // btnRicercaBinariaMatricola
+            // 
+            this.btnRicercaBinariaMatricola.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRicercaBinariaMatricola.Location = new System.Drawing.Point(17, 160);
+            this.btnRicercaBinariaMatricola.Name = "btnRicercaBinariaMatricola";
+            this.btnRicercaBinariaMatricola.Size = new System.Drawing.Size(163, 42);
+            this.btnRicercaBinariaMatricola.TabIndex = 5;
+            this.btnRicercaBinariaMatricola.Text = "RICERCA BINARIA MATRICOLA";
+            this.btnRicercaBinariaMatricola.UseVisualStyleBackColor = true;
+            this.btnRicercaBinariaMatricola.Click += new System.EventHandler(this.btnRicercaBinariaMatricola_Click);
+            // 
+            // btnCercaClassePiùStudenti
+            // 
+            this.btnCercaClassePiùStudenti.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCercaClassePiùStudenti.Location = new System.Drawing.Point(17, 115);
+            this.btnCercaClassePiùStudenti.Name = "btnCercaClassePiùStudenti";
+            this.btnCercaClassePiùStudenti.Size = new System.Drawing.Size(163, 42);
+            this.btnCercaClassePiùStudenti.TabIndex = 4;
+            this.btnCercaClassePiùStudenti.Text = "CERCA CLASSE CON + STUDENTI";
+            this.btnCercaClassePiùStudenti.UseVisualStyleBackColor = true;
+            this.btnCercaClassePiùStudenti.Click += new System.EventHandler(this.btnCercaClassePiùStudenti_Click);
+            // 
+            // btnContaStudentiClasse
+            // 
+            this.btnContaStudentiClasse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContaStudentiClasse.Location = new System.Drawing.Point(17, 67);
+            this.btnContaStudentiClasse.Name = "btnContaStudentiClasse";
+            this.btnContaStudentiClasse.Size = new System.Drawing.Size(163, 42);
+            this.btnContaStudentiClasse.TabIndex = 3;
+            this.btnContaStudentiClasse.Text = "CONTA STUDENTI CLASSE";
+            this.btnContaStudentiClasse.UseVisualStyleBackColor = true;
+            this.btnContaStudentiClasse.Click += new System.EventHandler(this.btnContaStudentiClasse_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(1162, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 208);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Query";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(17, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 42);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "RICERCA BINARIA MATRICOLA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(17, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "CERCA CLASSE CON + STUDENTI";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(17, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "CONTA STUDENTI CLASSE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(17, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 42);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "ORDINA STUDENTI NOMINATIVO";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // dgvValutazioni
+            // 
+            this.dgvValutazioni.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvValutazioni.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvValutazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvValutazioni.Location = new System.Drawing.Point(710, 47);
+            this.dgvValutazioni.Name = "dgvValutazioni";
+            this.dgvValutazioni.Size = new System.Drawing.Size(446, 150);
+            this.dgvValutazioni.TabIndex = 5;
+            // 
+            // btnCaricaValutazioni
+            // 
+            this.btnCaricaValutazioni.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaricaValutazioni.Location = new System.Drawing.Point(710, 12);
+            this.btnCaricaValutazioni.Name = "btnCaricaValutazioni";
+            this.btnCaricaValutazioni.Size = new System.Drawing.Size(163, 23);
+            this.btnCaricaValutazioni.TabIndex = 4;
+            this.btnCaricaValutazioni.Text = "CARICA VALUTAZIONI";
+            this.btnCaricaValutazioni.UseVisualStyleBackColor = true;
+            this.btnCaricaValutazioni.Click += new System.EventHandler(this.btnCaricaValutazioni_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dgvValutazioni);
+            this.Controls.Add(this.btnCaricaValutazioni);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvStudenti);
+            this.Controls.Add(this.btnCaricaTabellaStudenti);
+            this.Name = "frmMain";
+            this.Text = "Gestione Tabella";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudenti)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvValutazioni)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCaricaTabellaStudenti;
+        private System.Windows.Forms.DataGridView dgvStudenti;
+        private System.Windows.Forms.Button btnOrdinaStudentiCognomeNome;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnContaStudentiClasse;
+        private System.Windows.Forms.Button btnCercaClassePiùStudenti;
+        private System.Windows.Forms.Button btnRicercaBinariaMatricola;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dgvValutazioni;
+        private System.Windows.Forms.Button btnCaricaValutazioni;
+    }
+}
+
---
+++ 1. Informatica/C#/9. Visuale/GestioneTabella3B/frmMain.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace GetioneTabella3B
+{
+    public partial class frmMain : Form
+    {
+        static Random rnd = new Random();
+        public struct studente
+        {
+            public int matricola;
+            public string cognome;
+            public string nome;
+            public string classe;
+        }
+        string[] datiStudenti = new string[]
+        {
+            "Rossi Marco 1A", "Bianchi Luca 2B", 
+            "Ferrari Anna 2B", "Giordano Francesco 1D", 
+            "Colombo Silvia 2A", "Romano Paolo 2B", 
+            "Bianchi Maria 1A", "Rossi Giuseppe 2C", 
+            "Lombardi Alessandra 3A", "Costa Stefano 1B", 
+            "Russo Claudia 2D", "Esposito Michele 1A", 
+            "Martino Elena 2B", "DeLuca Carlo 2B", 
+            "Costa Giulia 3C", "Santoro Paolo 1A", 
+            "Galli Francesco 2A", "Ferrari Laura 1A", 
+            "Caruso Pietro 3B", "Vitale Caterina 2C", 
+            "Marchi Luca 2B", "Costa Federica 2B", 
+            "Fabbri Edoardo 1A", "Perri Anna 1A", 
+            "Neri Davide 2B", "Taddei Giorgia 3C", 
+            "Barone Roberto 2B", "Serra Valentina 2A", 
+            "Bianchini Luca 1A", "Cattaneo Alessandra 3B"
+        };
+        studente[] studenti;
+        int ns; //numero studenti
+        public struct valutazione
+        {
+            public string materia;
+            public int voto;
+            public DateTime data;
+            public int matricola; //chiave esterna
+        }
+        valutazione[] valutazioni;
+        string[] dativalutazioni = new string[]
+        {
+            "ITA 12/10/25","STO 12/10/25",
+            "INF 13/10/25","ITA 13/10/25",
+            "ING 13/10/25","STO 14/10/25",
+            "INF 14/10/25","INF 14/10/25",
+            "ITA 15/10/25","STO 15/10/25",
+            "INF 16/10/25","ITA 17/10/25",
+            "ING 17/10/25","STO 17/10/25",
+            "INF 17/10/25","INF 17/10/25",
+            "ITA 18/10/25","STO 18/10/25",
+            "INF 18/10/25","ITA 18/10/25",
+            "ING 20/10/25","STO 20/10/25",
+            "INF 21/10/25","INF 21/10/25",
+            "ITA 21/10/25","STO 21/10/25",
+            "INF 22/10/25","ITA 24/10/25",
+            "ING 24/10/25","STO 25/10/25",
+            "INF 26/10/25","INF 27/10/25",
+        };
+        int nv; //numero valutazioni
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCaricaTabellaStudenti_Click(object sender, EventArgs e)
+        {
+            if (!(int.TryParse(Interaction.InputBox("Inserisci il numero di studenti:"), out ns) && ns > 0))
+                MessageBox.Show("Devi inserire un valore positivo");
+            studenti = new studente[ns];
+            settaDgv(dgvStudenti, ns, 4);
+            caricaTabellaStudenti(studenti, datiStudenti);
+        }
+
+        private void caricaTabellaStudenti(studente[] studenti, string[] dati)
+        {
+            string[] dato = new string[3];
+            int x = rnd.Next(1, 1000);
+            for (int i = 0; i < studenti.Length; i++)
+            {
+                if (i == 0)
+                    studenti[i].matricola = x;
+                else
+                    studenti[i].matricola=studenti[i-1].matricola+rnd.Next(1, 10);
+                dato = dati[i].Split(' ');
+                studenti[i].cognome = dato[0];
+                studenti[i].nome = dato[1];
+                studenti[i].classe = dato[2];
+                scriviSuDgv(dgvStudenti, studenti[i], i);
+            }
+        }
+
+        private void scriviSuDgv(DataGridView dgv, studente studente, int i)
+        {
+            dgv.Rows[i].Cells[0].Value = studente.matricola.ToString();
+            dgv.Rows[i].Cells[1].Value = studente.cognome;
+            dgv.Rows[i].Cells[2].Value = studente.nome;
+            dgv.Rows[i].Cells[3].Value = studente.classe;
+            dgv.AutoResizeColumns();
+            dgv.AutoResizeRows();
+        }
+
+        private void settaDgv(DataGridView dgv, int r, int c)
+        {
+            dgv.RowCount = r;
+            dgv.ColumnCount = c;
+            dgv.ClearSelection();
+            dgv.RowHeadersVisible=false;
+            intestaTabella(dgvStudenti, c, "MATRICOLA,COGNOME,NOME,CLASSE");
+        }
+
+        private void intestaTabella(DataGridView dgv, int c, string msg)
+        {
+            string[] v=new string[c];
+            v=msg.Split(',');
+            for (int j = 0; j < c; j++)
+                dgv.Columns[j].HeaderText = v[j];
+        }
+
+        private void btnOrdinaStudentiCognomeNome_Click(object sender, EventArgs e)
+        {
+            ordinaStudentiNominativo(studenti);
+            visualTabellaStudenti(studenti, dgvStudenti);
+        }
+
+        private void visualTabellaStudenti(studente[] studenti, DataGridView dgv)
+        {
+            for (int i = 0; i < studenti.Length; i++)
+            {
+                dgv.Rows[i].Cells[0].Value = studenti[i].matricola.ToString();
+                dgv.Rows[i].Cells[1].Value = studenti[i].cognome;
+                dgv.Rows[i].Cells[2].Value = studenti[i].nome;
+                dgv.Rows[i].Cells[3].Value = studenti[i].classe;
+            }
+        }
+
+        private void ordinaStudentiNominativo(studente[] studenti)
+        {
+            //ordinamento per selezione
+            int posmin;
+            int n = studenti.Length;
+            string nominativo1 = null;
+            string nominativo2 = null;
+
+            for (int i = 0; i <= n-2; i++)
+            {
+                posmin = i;
+                for (int j = i+1; j <= n-1; j++)
+                {
+                    nominativo1 = studenti[posmin].cognome + studenti[posmin].nome;
+                    nominativo2 = studenti[j].cognome + studenti[j].nome;
+                    if (nominativo1.CompareTo(nominativo2) > 0)
+                        posmin = j;
+                }
+                if(i!=posmin)
+                    scambioRecord(ref studenti[i], ref studenti[posmin]);
+            }
+        }
+
+        private void scambioRecord(ref studente studente1, ref studente studente2)
+        {
+            studente aus;
+
+            aus = studente1;
+            studente1 = studente2;
+            studente2 = aus;
+        }
+
+        private void btnContaStudentiClasse_Click(object sender, EventArgs e)
+        {
+            //rottura di chiave
+            ordinaStudentiClasse(studenti);
+            visualTabellaStudenti(studenti,dgvStudenti);
+            string classe = Interaction.InputBox("Inserisci la classe");
+            MessageBox.Show("Gli studenti della classe " +
+                classe + " sono in totale: " + contaStudentiClasse(studenti, classe));
+        }
+
+        private void ordinaStudentiClasse(studente[] studenti)
+        {
+            //ordinamento per selezione
+            int posmin;
+            int n = studenti.Length;
+
+            for (int i = 0; i <= n - 2; i++)
+            {
+                posmin = i;
+                for (int j = i + 1; j <= n - 1; j++)
+                {
+                    if (studenti[posmin].classe.CompareTo(studenti[j].classe) > 0)
+                        posmin = j;
+                }
+                if (i != posmin)
+                    scambioRecord(ref studenti[i], ref studenti[posmin]);
+            }
+        }
+
+        private int contaStudentiClasse(studente[] studenti, string classe)
+        {
+            int n=studenti.Length;
+            int i = 0;
+            bool superato=false;
+            int cont = 0;
+            while (!superato && i <=n-1)
+            {
+                if (studenti[i].classe==classe)
+                {
+                    cont++;
+                    i++;
+                }
+                else
+                {
+                    if (studenti[i].classe.CompareTo(classe) > 0)
+                        superato=true;
+                    else
+                        i++;
+                }
+            }
+            return cont;
+        }
+
+        private void btnCercaClassePiùStudenti_Click(object sender, EventArgs e)
+        {
+            //cercare la classe con più studenti
+            ordinaStudentiClasse(studenti);
+            visualTabellaStudenti(studenti, dgvStudenti);
+            cercaClasseConPiùStudenti(studenti);
+        }
+
+        private void cercaClasseConPiùStudenti(studente[] studenti)
+        {
+            int cont = 1;
+            int max = int.MinValue;
+            string output = null;
+
+            for (int i = 0; i < studenti.Length-1; i++)
+            {
+                if (studenti[i].classe == studenti[i + 1].classe)
+                    cont++;
+                else //rompo la chiave
+                {
+                    if(cont>max)
+                    {
+                        max= cont;
+                        output = studenti[i].classe;
+                    }
+                    else
+                    {
+                        if (cont == max)
+                            output = output + " " + studenti[i].classe;
+                    }
+                    cont=1;
+                }
+            }
+            if (cont > max)
+            {
+                max = cont;
+                output = studenti[studenti.Length - 1].classe;
+            }
+            else
+            {
+                if (cont == max)
+                    output = output + " " + studenti[studenti.Length-1].classe;
+            }
+            MessageBox.Show($"La classe {output} ha {max.ToString()} studenti");
+        }
+
+        private void btnRicercaBinariaMatricola_Click(object sender, EventArgs e)
+        {
+            ordinaStudentiMatricola(studenti);
+            visualTabellaStudenti(studenti,dgvStudenti);
+            int matr = Convert.ToInt32(Interaction.InputBox("Inserisci la matricola"));
+            ricercaBinariaMatricola(studenti, matr);
+        }
+
+        private void ricercaBinariaMatricola(studente[] studenti, int matr)
+        {
+            int sup = 0;
+            int n=studenti.Length;
+            int inf = n - 1;
+            int metà;
+
+            do
+            {
+                metà = inf + sup / 2;
+                if (studenti[metà].matricola != matr)
+                {
+                    if (studenti[metà].matricola > matr)
+                        inf = metà - 1;
+                    else
+                        sup = metà + 1;
+                }
+            }
+            while (studenti[metà].matricola != matr && sup <= inf);
+            if (studenti[metà].matricola == matr)
+                MessageBox.Show(studenti[metà].cognome + "-" +
+                    studenti[metà].nome);
+            else
+                MessageBox.Show("Studente non trovato");
+        }
+
+        private void ordinaStudentiMatricola(studente[] studenti)
+        {
+            //ordinamento per selezione
+            int posmin;
+            int n = studenti.Length;
+
+            for (int i = 0; i <= n - 2; i++)
+            {
+                posmin = i;
+                for (int j = i + 1; j <= n - 1; j++)
+                {
+                    if (studenti[posmin].matricola>studenti[j].matricola) 
+                        posmin = j;
+                }
+                if (i != posmin)
+                    scambioRecord(ref studenti[i], ref studenti[posmin]);
+            }
+        }
+
+        private void btnCaricaValutazioni_Click(object sender, EventArgs e)
+        {
+            if (!(int.TryParse(Interaction.InputBox("Inserisci il numero di valutazioni:"), out nv) && nv > 0))
+            {
+                MessageBox.Show("Inserisci un valore positivo");
+            }
+            valutazioni = new valutazione[nv];
+            settaDgv(dgvValutazioni, nv, 4);
+            caricaTabellaValutazioni(valutazioni, dativalutazioni);
+        }
+
+        private void caricaTabellaValutazioni(valutazione[] valutazioni, string[] dati)
+        {
+            Random rnd = new Random();
+            string[] dato = new string[2];
+            for (int i = 0; i < studenti.Length; i++)
+            {
+                dato = dati[i].Split(' ');
+                valutazioni[i].materia = dato[0];
+                valutazioni[i].voto = rnd.Next(3,10);
+                //Garantisco integrità referenziale
+                valutazioni[i].matricola = cercaChiaveEsterna(studenti);
+                scriviSuDgv(dgvStudenti, studenti[i], i);
+            }
+        }
+
+        private int cercaChiaveEsterna(studente[] studenti)
+        {
+            Random rnd = new Random();
+            return studenti[rnd.Next(0, studenti.Length)].matricola;
+        }
+    }
+}
