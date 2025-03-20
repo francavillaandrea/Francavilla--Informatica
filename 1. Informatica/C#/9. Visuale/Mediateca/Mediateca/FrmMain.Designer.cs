2d4998a10caedec672261758d01703ab8eacc5ad
unknown <a.francavilla.3537@vallauri.net>
2025-03-20 10:23:14 +0100

    Oleg

---
+++ 1. Informatica/C#/9. Visuale/Mediateca/Mediateca/FrmMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Mediateca
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPageSoci = new System.Windows.Forms.TabPage();
+            this.tabPageMedia = new System.Windows.Forms.TabPage();
+            this.tabPageOperazioni = new System.Windows.Forms.TabPage();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.tabControlMain.SuspendLayout();
+            this.tabPageSoci.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPageSoci);
+            this.tabControlMain.Controls.Add(this.tabPageMedia);
+            this.tabControlMain.Controls.Add(this.tabPageOperazioni);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(818, 450);
+            this.tabControlMain.TabIndex = 0;
+            // 
+            // tabPageSoci
+            // 
+            this.tabPageSoci.Controls.Add(this.dgv);
+            this.tabPageSoci.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSoci.Name = "tabPageSoci";
+            this.tabPageSoci.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSoci.Size = new System.Drawing.Size(810, 424);
+            this.tabPageSoci.TabIndex = 0;
+            this.tabPageSoci.Text = "Soci";
+            this.tabPageSoci.UseVisualStyleBackColor = true;
+            // 
+            // tabPageMedia
+            // 
+            this.tabPageMedia.Location = new System.Drawing.Point(4, 22);
+            this.tabPageMedia.Name = "tabPageMedia";
+            this.tabPageMedia.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMedia.Size = new System.Drawing.Size(792, 424);
+            this.tabPageMedia.TabIndex = 1;
+            this.tabPageMedia.Text = "Media";
+            this.tabPageMedia.UseVisualStyleBackColor = true;
+            // 
+            // tabPageOperazioni
+            // 
+            this.tabPageOperazioni.Location = new System.Drawing.Point(4, 22);
+            this.tabPageOperazioni.Name = "tabPageOperazioni";
+            this.tabPageOperazioni.Size = new System.Drawing.Size(792, 424);
+            this.tabPageOperazioni.TabIndex = 2;
+            this.tabPageOperazioni.Text = "Operazioni";
+            this.tabPageOperazioni.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dgv.Location = new System.Drawing.Point(3, 3);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(634, 418);
+            this.dgv.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 450);
+            this.Controls.Add(this.tabControlMain);
+            this.Name = "FrmMain";
+            this.Text = "Mediateca";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPageSoci.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPageSoci;
+        private System.Windows.Forms.TabPage tabPageMedia;
+        private System.Windows.Forms.TabPage tabPageOperazioni;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ 1. Informatica/C#/9. Visuale/Mediateca/Mediateca/clsSoci.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateca
+{
+    public struct Socio
+    {
+        public string codSocio;
+        public string cognome;
+        public string nome;
+        public string email;
+        public string telefono;
+    }
+   
+    internal class clsSoci
+    {
+        public static Socio[] soci = new Socio[50];
+        public static string[] datiSoci = new string[]
+        {
+            "s1,Rossi,Mario,mario@rossi.it,3246785431",
+            "s2,Brambilla,Lorenzo,lorenzo@brambilla.it,3298765430",
+            "s3,Bianchi,Roberta,roberta@bianchi.it,3476722198",
+            "s4,Ferrero,Luigi,luigi@ferrero.it,3337651239",
+            "s5,Montessori,Maria,maria@montessori.it,3270985632",
+            "s6,Abate,Anna,anna@abate.it,3984560021",
+            "s7,Pautasso,Giuseppe,giuseppe@pautasso.it,3126754492",
+        };
+
+        public static int nSoci;
+
+        internal static void caricaSoci(object dgvSoci)
+        {
+            caricaTabellaSoci();
+            visualizzaTabellaSoci(dgvSoci);
+        }
+
+        private static void visualizzaTabellaSoci(object dgvSoci)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void caricaTabellaSoci()
+        {
+            for (int i = 0; i < datiSoci.Length; i++)
+            {
+                string[] dati = datiSoci[i].Split(',');
+                soci[i].codSocio = dati[0];
+                soci[i].cognome = dati[1];
+                soci[i].nome = dati[2];
+                soci[i].email= dati[3];
+                soci[i].telefono= dati[4];
+            }
+        }
+    }
+}
