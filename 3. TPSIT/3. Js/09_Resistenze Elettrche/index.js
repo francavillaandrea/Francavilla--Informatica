0d37ce6b12037b15709b317f9fbb085509bd61a7
Andrea Francavilla <a.francavilla.3537@vallauri.edu>
2025-03-21 13:25:03 +0100

    commit

--- 3. TPSIT/3. Js/09_Resistenze Elettrche/index.js
+++ 3. TPSIT/3. Js/09_Resistenze Elettrche/index.js
@@ -6,3 +6,14 @@ const colori = ["Argento", "Oro", "Nero", "Marrone", "Rosso", "Arancio", "Giallo
 const tolleranze=["10", "5", "", "1", "2", "", "", "0.5", "0.25", "0.1", "", ""];
 
 
+let _lstCifra1 = document.getElementById("lstCifra1");
+let _lstCifra2 = document.getElementById("lstCifra2");
+let _lstFattore = document.getElementById("lstFattore");
+let _lstTolleranza = document.getElementById("lstTolleranza");
+let _txtRisultato = document.getElementById("txtRisultato");
+
+
+function calcola()
+{
+    
+}
